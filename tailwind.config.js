a6520f944571a347a740e57f8e253ac2efcfde11
Daniella <daniellatsing@gmail.com>
2024-06-05 17:02:33 -0700

    add color palette, fonts, and plugins to configuration and global css, create files for home page, and download dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,42 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      'white': '#fbfdfe',
+      'feldgrau': '#425a4a',
+      'french-gray': '#d9d9d9',
+      'silver': '#b9b9b9',
+      'paynes-gray': '#5c657a',
+      'night': '#0b100d',
+      'davys-grey': '#4e4e4e',
+      'charcoal': '#3d5364',
+    },
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"]
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
 }
 

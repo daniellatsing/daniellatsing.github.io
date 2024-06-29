7b3918b5e8ab0b50716ec9970363824bae8557c3
Daniella <daniellatsing@gmail.com>
2024-06-28 23:44:45 -0700

    complete navbar with floating navbar and adjust social media in footer

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,21 +1,86 @@
 import Image from 'next/image';
 import Logo from "../logo.svg";
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';import { FloatingNav } from './ui/floating-navbar';
+
+import React, { useState } from 'react';
 
 export default function NavBar() {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    const navItems = [
+        {
+            name: "home",
+            link: "/"
+        },
+        {
+            name: "about",
+            link: "/about"
+        },
+        {
+            name: "works",
+            link: "/works"
+        }
+    ];
+
+    const worksItems = [
+        { 
+            name: "HerHeartCo", 
+            link: "/works/herheartco" 
+        },
+        { 
+            name: "Garden Time", 
+            link: "/works/garden-time" 
+        },
+        { 
+            name: "Muslim Student Association", 
+            link: "/works/msa" 
+        },
+        { 
+            name: "Carmax", 
+            link: "/works/carmax" 
+        },
+        { 
+            name: "Dear.m Journal", 
+            link: "/works/dear-m-journal" 
+        }
+    ];
 
     return (
-        <div className="w-full font-satoshi text-2xl container mx-auto font-general-regular flex place-content-end items-center mt-14 mb-12 sm:mb-28">
+        <nav className="w-full font-satoshi text-2xl container mx-auto font-general-regular flex place-content-end items-center mt-14 mb-12 sm:mb-28">
             <div className="absolute left-16 h-20 w-20">
                 <a href="/">
                     <Image src={Logo} alt="Logo" layout="fill" objectFit="contain" />
                 </a>
             </div>
-            <div className="flex justify-center gap-4 sm:gap-8 items-center font-bold text-2xl text-night">
+            <div className="flex justify-center sm:gap-8 items-center font-bold text-2xl text-night">
                 <a href="/" className="hover:text-indigo-500 dark:hover:text-indigo-400 transition duration-300 ease-in-out transform hover:-translate-y-1">home</a>
-                <a href="#about" className="hover:text-indigo-500 dark:hover:text-indigo-400 transition duration-300 ease-in-out transform hover:-translate-y-1">about</a>
-                <a href="#experience" className="hover:text-indigo-500 dark:hover:text-indigo-400 transition duration-300 ease-in-out transform hover:-translate-y-1">experience</a>
-                <a href="#works" className="hover:text-indigo-500 dark:hover:text-indigo-400 transition duration-300 ease-in-out transform hover:-translate-y-1">works</a>
+                <a href="/about" className="hover:text-indigo-500 dark:hover:text-indigo-400 transition duration-300 ease-in-out transform hover:-translate-y-1">about</a>
+                <div className="relative">
+                    <button type="button" id="menu-button" aria-expanded={dropdownOpen} aria-haspopup="true" onClick={toggleDropdown}>
+                        works
+                        <ExpandMoreRoundedIcon className="ml-1" />
+                    </button>
+                    {dropdownOpen && (
+                        <div className="absolute mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600" role="menu" aria-orientation="vertical" aria-labelledby="menu-button">
+                            <div className="py-2 text-sm text-night">
+                                {worksItems.map((item) => (
+                                    <a key={item.name} href={item.link} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white hover:text-indigo-500 dark:hover:text-indigo-400">
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
+                    )}
+                </div>
+                <button className="border border-davys-gray py-2 px-4 rounded-full shadow-sm transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md">
+                    <a href="https://drive.google.com/file/d/1aXOJIailit-5V0jgALfyzuRcnFywWBiG/view?usp=sharing" className="hover:text-indigo-500 dark:hover:text-indigo-400 self-center">resume</a>
+                </button>
             </div>
-        </div>
+            <FloatingNav navItems={navItems}/>
+        </nav>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { TypewriterEffect } from '@/app/components/ui/typewriter-effect';
-import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
 
 export default function Home() {
     const words = [
@@ -20,6 +19,7 @@ export default function Home() {
             className: "font-bold font-satoshi text-5xl text-feldgrau pb-10 mt-2"
         }
     ]
+    
     return ( 
         <main id="home" className="flex min-h-screen flex-col place-items-center py-24 font-satoshi">
             <div className="z-10 w-full max-w-7xl text-sm lg:flex">
@@ -28,14 +28,11 @@ export default function Home() {
             <div className="z-10 w-full max-w-7xl text-sm lg:flex">
                 <p className="font-bold text-2xl text-davys-grey pb-6">I am a Seattle-based software developer with a passion for designing intuitive and accessible solutions through the implementation through code and human-centered design.</p>
             </div>
-            {/* <div className="border border-feldgrau rounded-md px-6 py-6 hover:text-indigo-500 dark:hover:text-indigo-400 dark:bg-ternary-dark hover:text-indigo-500 dark:hover:text-indigo-400 dark:hover:bg-ternary-light shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-xl -start">
-                <a href="mailto:daniellatsing@gmail.com" id="intro-contact" className="flex flex-row text-feldgrau font-bold text-base text-xl place-items-center">
-                    <EmailRoundedIcon className=""/>
-                    <p className="px-2">Send a Message!</p>
-                </a>
-            </div> */}
-            {/* <div className="w-full mt-52 border border-french-gray dark:border-secondary-dark" /> */}
-            <div className="w-full mt-8 border border-french-gray dark:border-secondary-dark" />
+            <div className="w-full border border-french-gray dark:border-secondary-dark mt-16 mb-28" />
+            <div id="works" className="z-10 w-full max-w-7xl lg:flex">
+                <p className="text-5xl text-feldgrau font-bold">works</p>
+            </div>
+            <div className="w-full border border-french-gray dark:border-secondary-dark mt-24 mb-52" />
         </main>
   );
 }
